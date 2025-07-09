e18f50e6c5171b2dab4cf3ba3065f4b6d9fadc76
Svegiary <haristsitsirigos@gmail.com>
2025-07-09 21:11:36 +0300

    renamed intro component to text component and refactored styles

--- src/components/ui/lesson/index.tsx
+++ src/components/ui/lesson/index.tsx
@@ -3,5 +3,6 @@ export { default as LessonNavigation } from '../exercises/lesson-navigation';
 export { default as LessonProgressBar } from './lesson-progress-bar';
 export { default as PageTemplate } from './page-template';
 export { default as ContentRenderer } from './content-renderer';
-export { default as IntroComponent } from './intro-component';
+export { default as TextComponent } from './text-component';
 export { default as ConjugationTable } from './conjugation-table';
+export { VocabularyViewer } from './VocabularyViewer';
--- src/components/ui/lesson/text-component.tsx
+++ src/components/ui/lesson/text-component.tsx
@@ -2,15 +2,15 @@
 
 import React from 'react';
 
-interface IntroComponentProps {
+interface TextComponentProps {
   title: string;
   content: string;
   className?: string;
 }
 
-export const IntroComponent: React.FC<IntroComponentProps> = ({ title, content, className = '' }) => {
+export const TextComponent: React.FC<TextComponentProps> = ({ title, content, className = '' }) => {
   return (
-    <div className={`intro-component ${className}`}>
+    <div className={`text-component ${className}`}>
       <h3 className="text-lg font-serif text-roman-red mb-2">{title}</h3>
       <div
         className="p-4 bg-roman-parchment rounded-lg prose dark:prose-invert max-w-none prose-p:my-2 prose-p:leading-snug prose-headings:my-4 prose-ul:my-2 prose-ol:my-2"
@@ -20,4 +20,4 @@ export const IntroComponent: React.FC<IntroComponentProps> = ({ title, content, 
   );
 };
 
-export default IntroComponent;
+export default TextComponent;
