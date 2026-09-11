c2e166e37102b3f67ce38eb4392694fbb3fc44b4
Charalampos Tsitsiringos <haristsitsirigos@gmail.com>
2026-09-11 09:30:54 +0300

    Add sign-in stall diagnostics

--- tests/helpers/sentryMock.ts
+++ tests/helpers/sentryMock.ts
@@ -1,19 +1,25 @@
 export const captureException = jest.fn();
 export const captureMessage = jest.fn();
+export const addBreadcrumb = jest.fn();
 export const setUser = jest.fn();
+export const withScope = jest.fn((callback: (scope: { setUser: typeof setUser }) => unknown) => callback({ setUser }));
 export const setTag = jest.fn();
 export const init = jest.fn();
 export const replayIntegration = jest.fn(() => ({}));
 export const captureRouterTransitionStart = jest.fn();
 export const captureRequestError = jest.fn();
 
-export default {
+const sentryMock = {
   captureException,
   captureMessage,
+  addBreadcrumb,
   setUser,
+  withScope,
   setTag,
   init,
   replayIntegration,
   captureRouterTransitionStart,
   captureRequestError,
 };
+
+export default sentryMock;
